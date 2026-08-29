91e36b9451699174425781d5d4b71d4a444a91b8
Shritej KV <151450693+shritej1808@users.noreply.github.com>
2026-08-29 17:07:24 +0530

    Time: 5 ms (40.78%) | Memory: 47.4 MB (7.15%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.java
+++ 921-spiral-matrix-iii/spiral-matrix-iii.java
@@ -1,39 +1,37 @@
 class Solution {
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int[][] result=new int[rows*cols][2];
-        int index=0;
-        int[] dr={0,1,0,-1};
-        int[] dc={1,0,-1,0};
-        int r = rStart;
-        int c = cStart;
-        int direction=0;
-        int steps=1;
-
-        result[index][0] = r;
-        result[index][1] = c;
-        index++;
-        while(index< rows*cols){
-            for(int count=0;count<2;count++){
-                for(int i=0;i<steps;i++){
-                    r+=dr[direction];
-                    c+=dc[direction];
-
-                    if(r>=0 && r<rows && c>=0 && c<cols){
-                        result[index][0]=r;
-                        result[index][1]=c;
-                        index++;
-                    }
-                    if(index==rows*cols){
-                        return result;
-                    }
-
+    int[][] result=new int[rows*cols][2];
+    int index=0;
+    int r=rStart;
+    int c=cStart;
+    int[] dr={0,1,0,-1};
+    int[] dc={1,0,-1,0};
+    int direction=0;
+    int steps=1;
+    result[index][0]=r;
+    result[index][1]=c;
+    index++;
+    while(index<rows*cols){
+        for(int count=0;count<2;count++){
+            for(int i=0;i<steps;i++){
+                r+=dr[direction];
+                c+=dc[direction];
+                if(r>=0 && r<rows && c>=0 && c<cols){
+                    result[index][0]=r;
+                    result[index][1]=c;
+                    index++;
                 }
-                direction=(direction+1)%4;
+                if(index==rows*cols){
+                    return result;
+                }
             }
-             steps++; 
-            }
-           
-            return result;
+            direction=(direction+1)%4;
         }
+        steps++;
+    }   
+    return result;
+    
+    
+    }
      
 }
