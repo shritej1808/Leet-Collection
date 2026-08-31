1c56067918309e08dc91daa5ba26e3b7f0c001a0
Shritej KV <151450693+shritej1808@users.noreply.github.com>
2026-08-31 20:30:15 +0530

    Time: 1 ms (20.07%) | Memory: 43.1 MB (80.62%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,62 +1,44 @@
 class Solution {
     public void gameOfLife(int[][] board) {
+        int m=board.length;
+        int n=board[0].length;
 
-        int m = board.length;
-        int n = board[0].length;
-
-        int[][] copy = new int[m][n];
-
-        // Copy current board
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                copy[i][j] = board[i][j];
-            }
-        }
-        // Check every cell
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                int liveNeighbors = 0;
-
-                // Check 8 directions
-                for (int x = i - 1; x <= i + 1; x++) {
-                    for (int y = j - 1; y <= j + 1; y++) {
-
-                        // Skip current cell
-                        if (x == i && y == j) {
-                            continue;
-                        }
-
-                        // Check boundaries
-                        if (x >= 0 && x < m && y >= 0 && y < n) {
-                            if (copy[x][y] == 1) {
-                                liveNeighbors++;
-                            }
-                        }
-                    }
-                }
-
-                // Apply rules
-                if (copy[i][j] == 1) {
-
-                    // Live cell dies
-                    if (liveNeighbors < 2 || liveNeighbors > 3) {
-                        board[i][j] = 0;
-                    }
-
-                    // Live cell stays alive for 2 or 3 neighbors
-                    else {
-                        board[i][j] = 1;
-                    }
-
-                } else {
-
-                    // Dead cell becomes alive with exactly 3 neighbors
-                    if (liveNeighbors == 3) {
-                        board[i][j] = 1;
-                    }
+         int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
+for(int i=0;i<m;i++){
+    for(int j=0;j<n;j++){
+        int liveNeighbours=0;
+        for(int k=0;k<8;k++){
+            int nr=i+dr[k];
+            int nc=j+dc[k];
+            if(nr>=0 && nr<m && nc>=0 && nc<n ){
+                if(board[nr][nc]==1 || board[nr][nc]==2){
+                    liveNeighbours++;
                 }
             }
         }
+        if(board[i][j]==1){
+            if(liveNeighbours<2 || liveNeighbours>3){
+                board[i][j]=2;
+            }
+        }
+        if(board[i][j]==0){
+            if(liveNeighbours==3){
+                board[i][j]=3;
+            }
+        }
+    }
+}
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==2){
+                    board[i][j]=0;
+                }
+                else if(board[i][j]==3){
+                    board[i][j]=1;
+                }
+            }
+        }
+
     }
 }
