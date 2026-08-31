e85a1f0fa5100e90a411cf895c4d3d4860b1cf62
Shritej KV <151450693+shritej1808@users.noreply.github.com>
2026-08-31 20:54:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (64.50%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -5,36 +5,41 @@ class Solution {
 
          int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
         int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
-for(int i=0;i<m;i++){
-    for(int j=0;j<n;j++){
-        int liveNeighbours=0;
-        for(int k=0;k<8;k++){
-            int nr=i+dr[k];
-            int nc=j+dc[k];
-            if(nr>=0 && nr<m && nc>=0 && nc<n ){
-                if(board[nr][nc]==1 || board[nr][nc]==2){
-                    liveNeighbours++;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int liveNeighbours=0;
+                for(int k=0;k<8;k++){
+                    int nr=i+dr[k];
+                    int nc=j+dc[k];
+
+                    if(nr>=0 && nr<m && nc>=0 && nc<n){
+                        if(board[nr][nc]==1 || board[nr][nc]==2){
+                            liveNeighbours++;
+                    
                 }
+                        
+                    }
+                }
+                if(board[i][j]==1){
+if(liveNeighbours<2 || liveNeighbours>3){
+                    board[i][j]=2;
+                }
+                }
+                
+                if(board[i][j]==0){
+                    if(liveNeighbours==3){
+                    board[i][j]=3;
+                }
+                }
+                
             }
         }
-        if(board[i][j]==1){
-            if(liveNeighbours<2 || liveNeighbours>3){
-                board[i][j]=2;
-            }
-        }
-        if(board[i][j]==0){
-            if(liveNeighbours==3){
-                board[i][j]=3;
-            }
-        }
-    }
-}
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]==2){
                     board[i][j]=0;
                 }
-                else if(board[i][j]==3){
+                if(board[i][j]==3){
                     board[i][j]=1;
                 }
             }
