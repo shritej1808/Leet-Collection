751a83a957e1aee23aa40c6181ff05fcbf597193
Shritej KV <151450693+shritej1808@users.noreply.github.com>
2026-09-02 18:32:30 +0530

    Time: 3 ms (74.12%) | Memory: 47.3 MB (68.78%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.java
+++ 875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -11,6 +11,7 @@ class Solution {
             if(arr[i]>arr[i-1]){
                 left[i]+=left[i-1];
             }
+
         }
         for(int i=n-2;i>=0;i--){
             if(arr[i]>arr[i+1]){
