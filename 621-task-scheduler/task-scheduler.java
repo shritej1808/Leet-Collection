24e81f5ed63dd5ba094b49a5979972281d81e0cc
Shritej KV <151450693+shritej1808@users.noreply.github.com>
2026-09-04 19:55:39 +0530

    Time: 13 ms (60.31%) | Memory: 47.5 MB (96.90%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -4,17 +4,17 @@ class Solution {
         for(char c: tasks){
             map.merge(c,1,Integer::sum);
         }
-        int maxfreq=0;
+        int freqMax=0;
         for(int freq: map.values()){
-            maxfreq=Math.max(maxfreq,freq);
+            freqMax=Math.max(freqMax,freq);
         }
         int maxCount=0;
-        for(int freq:map.values()){
-            if(freq==maxfreq){
+        for(int freq: map.values()){
+            if(freq==freqMax){
                 maxCount++;
             }
         }
-        int result=(maxfreq-1)*(n+1)+maxCount;
+        int result=(freqMax-1)*(n+1)+maxCount;
         return Math.max(result,tasks.length);
     }
 }
