698cd2932e5a63a238425c49cfb346cb934a36ad
Shritej KV <151450693+shritej1808@users.noreply.github.com>
2026-09-06 12:47:03 +0530

    Time: 70 ms (16.59%) | Memory: 47.6 MB (55.27%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -7,13 +7,12 @@ class Solution {
             while(set.contains(s.charAt(right))){
                 set.remove(s.charAt(left));
                 left++;
-
             }
             set.add(s.charAt(right));
-
-max=Math.max(max,right-left+1);
+            max=Math.max(max,right-left+1);
         }
-        return max;
+return max;
+
         
     }
 }
