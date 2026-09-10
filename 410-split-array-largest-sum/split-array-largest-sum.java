e0b015aa2dc5efa467596def3fd1b77c44097739
Shritej KV <151450693+shritej1808@users.noreply.github.com>
2026-09-10 23:10:04 +0530

    Time: 2 ms (31.86%) | Memory: 42.9 MB (83.90%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,41 +1,34 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
 
-        int left = 0;
-        int right = 0;
-
-        // Find search range
-        for (int num : nums) {
-            left = Math.max(left, num);
-            right += num;
+        int left=0;
+        int right=0;
+        for(int num:nums){
+            left=Math.max(left,num);
+            right+=num;
         }
-
-        while (left < right) {
-
-            int mid = left + (right - left) / 2;
-
-            int subarrays = 1;
-            int currentSum = 0;
-
-            for (int num : nums) {
-
-                if (currentSum + num > mid) {
+        while(left<right){
+            int mid=left+(right-left)/2;
+            int currSum=0;
+            int subarrays=1;
+            for(int num:nums){
+                if(currSum+num>mid){
                     subarrays++;
-                    currentSum = num;
-                } else {
-                    currentSum += num;
+                    currSum=num;
+                }
+                else{
+                    currSum+=num;
                 }
             }
-
-            if (subarrays <= k) {
-                // mid works, try smaller
-                right = mid;
-            } else {
-                // mid is too small
-                left = mid + 1;
+            if(subarrays<=k){
+                right=mid;
             }
-        }
+            else{
+                left=mid+1;
+            }
 
+
+        }
         return left;
     }
 }
